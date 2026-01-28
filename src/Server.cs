f77c3b5030f31abb62138896bffb5ac0150a728d
jason goodney <jason.goodney@gmail.com>
2026-01-28 15:50:11 -0500

    response with body

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -13,12 +14,12 @@ while (true) {
 
     Byte[] buffer = new Byte[256];
     stream.Read(buffer, 0, buffer.Length);
-    String data = System.Text.Encoding.ASCII.GetString(buffer);
-    if (data == "") {
+    String request = System.Text.Encoding.ASCII.GetString(buffer);
+    if (request == "") {
         continue;
     }
 
-    String[] fields = data.Split("\r\n");
+    String[] fields = request.Split("\r\n");
     if (fields.Length == 0) {
         continue;
     }
@@ -26,8 +27,23 @@ while (true) {
     String[] requestLine = fields[0].Split(' ');
     String response = "HTTP/1.1 404 Not Found\r\n\r\n";
     if (requestLine.Length == 3) {
-        if (requestLine[1] == "/") {
+        String method = requestLine[0];
+        String requestTarget = requestLine[1];
+        String protocol = requestLine[2];
+
+        if (requestTarget == "/") {
             response = "HTTP/1.1 200 OK\r\n\r\n";
+        }
+        else if (requestTarget.StartsWith("/echo")) {
+            String[] components = requestTarget.Split('/');
+            String content = components[components.Length-1];
+            StringBuilder sb = new StringBuilder(128);
+            sb.Append("HTTP/1.1 200 OK\r\n");
+            sb.Append("Content-Type: text/plain\r\n");
+            sb.Append($"Content-Length: {content.Length}\r\n");
+            sb.Append("\r\n");
+            sb.Append(content);
+            response = sb.ToString();
         }
     }
 
